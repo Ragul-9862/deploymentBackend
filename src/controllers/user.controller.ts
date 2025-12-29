9f6148d24c5299eacdfd31e6ab776dfe936363a9
ragul-9862 <ragulragul9862@gmail.com>
2025-12-29 13:32:42 +0530

    Title Update-latest

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { User } from "../models/user.model";
 
-// GET users from MongoDB
+
 export const getUsers = async (req: Request, res: Response) => {
   try {
     const users = await User.find();
